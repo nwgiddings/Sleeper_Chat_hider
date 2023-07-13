aa63881d85c5d1116f421bc83c00101967587336
Nash Giddings <nwgiddings@gmail.com>
2023-07-13 17:30:45 -0400

    Update sleeper url from .app to .com

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'sleeper.app'},
+          pageUrl: {hostEquals: 'sleeper.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
